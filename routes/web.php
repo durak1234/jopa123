c90b147ed5d8e31387e0c390587364f5ee28e22f
Абылай Абен <ablaiaben@hmail.com>
2024-12-06 15:44:00 +0500

    Initial commit

---
+++ routes/web.php
@@ -0,0 +1,57 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+use App\Models\Skill;
+
+use App\Models\Portfolio;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route:: get ('/test', function(){
+    return 123;
+});
+
+Route::get('/skills/{category}', function($category){
+    $title = "Навыки в категории $category";
+    $skills = Skill::where('category', $category)->get();
+    return view('skills')
+        ->with('title',$title)
+        ->with('skills',$skills);
+});
+
+Route:: get ('/skills', function(){
+    $title = 'Навыки';
+    $skills = Skill::all();
+    return view('skills')
+        ->with('title', $title)
+        ->with('skills',$skills);
+});
+
+Route:: get ('/portfolio', function(){
+    $title = 'Портфолио Terricon';
+    $portfolio = Portfolio::all();
+    return view('portfolio')
+        ->with('title', $title)
+        ->with('portfolio', $portfolio);
+});
+
+Route:: get ('/news', function(){
+    $title = 'Новости Terricon';
+    return view('news', [
+        'title' => $title
+    ]);
+});
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
